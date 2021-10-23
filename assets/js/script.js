7e72f1ea45a7c50c875857fde54676aae3048f08
SMTamim <30808870+SMTamim@users.noreply.github.com>
2021-10-23 15:58:27 +0600

    login and Signup done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,74 @@
 window.onload = function() {
     let dashboard_nav = document.querySelector('.secondary_nav');
+    if(dashboard_nav != null){
+        var links = dashboard_nav.querySelectorAll("ul>li>a");
+        for (let i = 0; i < links.length; i++) {
+            links[i].addEventListener("click", function() {
+                if(links[i].getAttribute('href')=="#doctor_approval"){
+                    hide_all_except_clicked(links[i].getAttribute('href'));
+                }else if((links[i].getAttribute('href')=="#departments")){
+                    hide_all_except_clicked(links[i].getAttribute('href'));
+                    
+                }else if((links[i].getAttribute('href')=="#all_patitents")){
+                    hide_all_except_clicked(links[i].getAttribute('href'));
+                }else if((links[i].getAttribute('href')=="#approved_doctors")){
+                    hide_all_except_clicked(links[i].getAttribute('href'));
+                }else if((links[i].getAttribute('href')=="#add_another_dept")){
+                    hide_all_except_clicked(links[i].getAttribute('href'));
+                }
+            });
+        }
+    }
+    var go_back_in_signin_page = document.getElementById('goback');
+    if(go_back_in_signin_page != null){
+        go_back_in_signin_page.addEventListener('click', ()=>{
+            document.getElementById('signinORsignup').classList.add('show-single-item');
+            go_back_in_signin_page.classList.add('hide-single-item');
+            go_back_in_signin_page.classList.remove('show-single-item');
+            hide_all_forms_except(document.getElementById('signinORsignup'));
+        })
+    
+        let singInButton = document.getElementById('signinbtn')
+        singInButton.addEventListener('click', ()=>{
+            document.getElementById('signInForm').classList.add('show-single-item');
 
-    var links = dashboard_nav.querySelectorAll("ul>li>a");
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", function() {
-            if(links[i].getAttribute('href')=="#doctor_approval"){
-                hide_all_except_clicked(links[i].getAttribute('href'));
-            }else if((links[i].getAttribute('href')=="#departments")){
-                hide_all_except_clicked(links[i].getAttribute('href'));
-                
-            }else if((links[i].getAttribute('href')=="#all_patitents")){
-                hide_all_except_clicked(links[i].getAttribute('href'));
-            }else if((links[i].getAttribute('href')=="#approved_doctors")){
-                hide_all_except_clicked(links[i].getAttribute('href'));
-            }else if((links[i].getAttribute('href')=="#add_another_dept")){
-                hide_all_except_clicked(links[i].getAttribute('href'));
-            }
-        });
+            document.getElementById('signInForm').classList.remove('hide-single-item');
+            
+            go_back_in_signin_page.classList.add('show-single-item');
+            document.getElementById('signinORsignup').classList.add('hide-single-item');
+            document.getElementById('signinORsignup').classList.remove('show-single-item');
+    
+        })
+
+        let singUpButton = document.getElementById('signupbtn')
+        singUpButton.addEventListener('click', ()=>{
+            document.getElementById('signUpCheck').classList.add('show-single-item');
+
+            document.getElementById('signUpCheck').classList.remove('hide-single-item');
+            
+            go_back_in_signin_page.classList.add('show-single-item');
+            document.getElementById('signinORsignup').classList.add('hide-single-item');
+            document.getElementById('signinORsignup').classList.remove('show-single-item');
+        })
+
+        let chooseAccountType =  document.querySelectorAll('input[name="profession"]');
+
+        chooseAccountType.forEach(element=>{
+            element.addEventListener('click', ()=>{
+                let account_type = document.querySelector('input[name="profession"]:checked').value;
+                if(account_type == 'doctor'){
+                    hide_all_forms_except(document.getElementById('signup-doc'));
+                    go_back_in_signin_page.classList.add('show-single-item');
+                }else if(account_type == 'patient'){
+                    hide_all_forms_except(document.getElementById('signup-patient'));
+                    go_back_in_signin_page.classList.add('show-single-item');
+                }
+            })
+        })
+
+        let doc_degrees = document.getElementById('d-degrees');
+        let add_degree = document.getElementById('add-another-degree')
+
     }
 
 }
@@ -32,6 +84,20 @@ function hide_all_except_clicked(item_not_to_hide){
         else{
             element.classList.add('show-single-item');
             element.classList.remove('hide-single-item');
+        }
+    });
+}
+
+function hide_all_forms_except(item){
+    all_forms = document.querySelectorAll('.form-item');
+    all_forms.forEach(element => {
+        if(element.getAttribute('id') != item.getAttribute('id')){
+            element.classList.remove('show-single-item');
+            element.classList.add('hide-single-item');
+        }
+        else{
+            item.classList.add('show-single-item');
+            item.classList.remove('hide-single-item');
         }
     });
 }
--- sign-in-or-sign-up.php
+++ sign-in-or-sign-up.php
@@ -43,8 +43,8 @@
     </div>
     <div class="content_wrapper">
     <div class="wrapper">
-        <button id="goback" class="hide-form-sign">Go Back</button>
-            <div id="signinORsignup">
+        <button id="goback" class="btn btn-goback hide-single-item">Go Back</button>
+            <div id="signinORsignup" class="form-item">
                 <ul>
                     <li><button id="signinbtn" class="signin">Sign In</button></li>
                     <li>OR</li>
@@ -52,27 +52,37 @@
                 </ul>
             </div>
     
-            <div class="signInForm hide-single-item">
+            <div id="signInForm" class="hide-single-item form-item">
                 <form method="POST" id="signin" 
                 class="sign"> 
                 <fieldset>
                     <legend>Login</legend>
-                <label for="text"><h3>Username or Email:</h3></label>
-                    <input type="text" name="email" id="email" placeholder="Mr.x/ mrx@xyz.com">
-                    <label for="pass"><h3>Password</h3></label>
+                <label for="text"><h3>Enter your e-mail:</h3></label>
+                    <input type="email" name="email" id="email" placeholder="Mr.x/ mrx@xyz.com">
+                    <label for="pass"><h3>Enter your password</h3></label>
                     <input type="password" name="pass" id="pass" placeholder="Password">
-                    <input type="checkbox" name="rememberme" checked="">
-                    <span>Remember Me</span>
-                    <input type="submit" value="Sign In" name="signin">
+                    <span> 
+                        <input type="checkbox" name="rememberme" checked="">
+                        Remember Me
+                    </span>
+                    <input type="submit" value="Sign In" name="signin" class='btn btn-submit'>
                 </fieldset>    
                 </form>
+            </div>
 
-                    <label for="profession">
-                        <h2>Choose account type:</h2>
-                    </label>
-                    <input type="radio" name="profession" id="profession" value='doctor'> Doctor
-                    <input type="radio" name="profession" id="profession" value='patient' checked> Patient
-            <form method="POST" id="signup-patient" class="sign  hide-single-item"> 
+            <div id="signUpCheck" class="hide-single-item form-item">
+                <label for="profession">
+                    <h2>Choose account type:</h2>
+                </label>
+                <span>
+                    <input type="radio" name="profession" id="profession-doctor" value='doctor'> Doctor
+                </span>
+                <span>
+                    <input type="radio" name="profession" id="profession-patient" value='patient'> Patient
+                </span>
+                
+            </div>
+            <form method="POST" id="signup-patient" class="sign  hide-single-item form-item"> 
                 <fieldset>
                     <legend>Register Patient</legend>
                     <label for="email"><h3>Email:</h3></label>
@@ -94,26 +104,26 @@
                     <label for="p-cpass"><h3>Confirm Password</h3></label>
                     <input type="password" name="p-cpass" id="p-cpass">
     
-                    <input type="submit" name="signup" value="Register">
+                    <input type="submit" name="signup" value="Register"  class='btn btn-submit'>
                 </fieldset>
                 </form>
-                <form method="POST" id="signup-doc " class="sign hide-single-item"> 
+                <form method="POST" id="signup-doc" class="sign hide-single-item form-item"> 
                     <fieldset>
                         <legend>Register Doctor</legend>
-                        <label for="email"><h3>Email:</h3></label>
-                        <input type="email" name="p-email" id="p-email" placeholder="yourmail@xyz.com" required="">
+                        <label for="d-email"><h3>Email:</h3></label>
+                        <input type="email" name="d-email" id="d-email" placeholder="yourmail@xyz.com" required="">
         
         
-                        <label for="name"><h3>Full Name:</h3></label>
-                        <input type="text" name="p-name" id="p-name" placeholder="Mr. X" required="">
+                        <label for="d-name"><h3>Full Name:</h3></label>
+                        <input type="text" name="d-name" id="d-name" placeholder="Mr. X" required="">
         
-                        <label for="p-phpne"><h3>Phone No:</h3></label>
-                        <input type="tel" name="p-phone" id="p-phone" pattern="{0-9}[11]">
+                        <label for="d-phpne"><h3>Phone No:</h3></label>
+                        <input type="tel" name="d-phone" id="d-phone" pattern="{0-9}[11]">
                         
                         <label for="d-department">
                             <h3>Select your department:</h3>
                         </label>
-                        <select name="department" id="department">
+                        <select name="d-department" id="department">
                             <option value="medicine">Medicine</option>
                             <option value="gyno">Gynocololy</option>
                             <option value="neural">Neural</option>
@@ -123,31 +133,47 @@
                         <label for="doc-edu">
                             <h3>Please enter your educational qualifications:</h3>
                         </label>
-                        <div id="degrees">
-                            <input type="text" name="deg1" id="degree"> <button id="add-another-degree">Add Another</button>
+                        <div id="d-degrees">
+                            <input type="text" name="deg1" id="degree" class="doc-degree"> <button id="add-another-degree" class="btn btn-add-another-field">Add Another</button>
                         </div>
                         
                         <br>
-                    <label for="select_available_days">
-                        <h2>Select your available days:</h2>
-                    </label>
-                    <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="0"> Saturday <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="1"> Sunday <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="2"> Monday <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="3"> Tuesday <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="4"> Wednessday <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="5"> Thursday <br>
-                    <input type="checkbox" name="day_available[]" id="routine" value="6"> Friday
+                        <div id="available_doc_days">
+                            <label for="select_available_days">
+                                <h2>Select your available days:</h2>
+                            </label>
+                            <span>
+                                Saturday <input type="checkbox" name="day_available[]" value="0"> 
+                            </span>
+                            <span>
+                                Sunday <input type="checkbox" name="day_available[]"  value="1">  
+                            </span>
+                            <span>
+                                Monday <input type="checkbox" name="day_available[]" value="2">
+                            </span>
+                            <span>
+                                Tuesday <input type="checkbox" name="day_available[]" value="3">  
+                            </span>
+                            <span>
+                                Wednessday <input type="checkbox" name="day_available[]" value="4"> 
+                            </span>
+                            
+                            <span>
+                                Thursday <input type="checkbox" name="day_available[]" value="5"> 
+                            </span>
+                            <span>
+                                Friday <input type="checkbox" name="day_available[]" value="6">
+                            </span> 
+                        </div>
                         
 
-                        <label for="p-pass"><h3>Password</h3></label>
-                        <input type="password" name="p-pass" id="p-pass">
+                        <label for="d-pass" id="d-pass"><h3>Password</h3></label>
+                        <input type="password" name="d-pass" >
         
-                        <label for="p-cpass"><h3>Confirm Password</h3></label>
-                        <input type="password" name="p-cpass" id="p-cpass">
+                        <label for="d-cpass"><h3>Confirm Password</h3></label>
+                        <input type="password" name="d-cpass" id="d-cpass">
         
-                        <input type="submit" name="signup" value="Register">
+                        <input type="submit" name="signup" value="Register"  class='btn btn-submit' class='btn btn-submit'>
                     </fieldset>
                     </form>
             </div>
