1cff646a4dc145652d53e5f3f23fec09ec3d9337
Muzahidul Islam <muzahidul190@gmail.com>
2021-10-29 15:33:00 +0600

    about.php title changed

--- about.php
+++ about.php
@@ -35,7 +35,7 @@ if($count > 0){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doctor Details | DBMS Hospital</title>
+    <title>About Us | DBMS Hospital</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
     <script src="assets/js/jquery.min.js"></script>
